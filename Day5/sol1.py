dcb2398c71c5f7ed22b819b78384b4574a07eba1
Saif Ur Rasool <rasoolsaif24@gmail.com>
2025-12-05 11:40:49 +0500

    Update sol1.py

---
+++ Day5/sol1.py
@@ -0,0 +1,30 @@
+def count_fresh_ingredients(path="input.txt") -> int:
+    ranges = []
+    available_ids = []
+    reading_ranges = True
+
+    with open(path) as f:
+        for line in f:
+            line = line.strip()
+            if line == "":
+                reading_ranges = False
+                continue
+
+            if reading_ranges:
+                start, end = map(int, line.split("-"))
+                ranges.append((start, end))
+            else:
+                available_ids.append(int(line))
+
+    fresh_count = 0
+    for ingredient in available_ids:
+        for start, end in ranges:
+            if start <= ingredient <= end:
+                fresh_count += 1
+                break   # no need to check more ranges
+
+    return fresh_count
+
+
+if __name__ == "__main__":
+    print(count_fresh_ingredients("input.txt"))
